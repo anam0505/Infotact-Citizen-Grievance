da1f284cb8a880eca3378595d25c41d35085b8fd
juspratik <pjexam824@gmail.com>
2026-05-18 22:46:01 +0530

    data-sourcing: added missing value and duplicate reporting

--- src/load_data.py
+++ src/load_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 def load_data(filepath):
     try:
         df = pd.read_csv(filepath, encoding='utf-8')
+
     except UnicodeDecodeError:
         print("UTF-8 failed, trying latin1...")
         df = pd.read_csv(filepath, encoding='latin1')
@@ -18,9 +19,15 @@ def load_data(filepath):
     print("\nFirst 5 Rows:")
     print(df.head())
 
+    print("\nMissing Values:")
+    print(df.isnull().sum())
+
+    print("\nDuplicate Rows:")
+    print(df.duplicated().sum())
+
     return df
 
 
 if __name__ == "__main__":
-    filepath = "data_/raw/raw/municipal_training_set_1100.csv"
+    filepath = "data/raw/municipal_training_set_1100.csv"
     df = load_data(filepath)
